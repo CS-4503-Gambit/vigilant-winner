a9de354a1a66ffdba226abc10555157b6a00b728
Jon Bolin <jonb377@gmail.com>
2017-10-26 14:01:27 -0500

    Updated registrar urls.
    Redirect for Registrar works now

--- Code/heartland/heartland/urls.py
+++ Code/heartland/heartland/urls.py
@@ -19,6 +19,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^core/', include('core.urls')),
+    url(r'^registrar/', include('registrar.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', auth_views.login, {'template_name': 'core/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
--- Code/heartland/registrar/urls.py
+++ Code/heartland/registrar/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^home$', view.home, name='home'),
+    url(r'^home$', views.home, name='home'),
 ]
