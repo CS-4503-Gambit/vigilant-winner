eee793825c07d31b1be834bf26481b433e84ea24
Jon Bolin <jonb377@gmail.com>
2017-11-06 20:27:26 -0600

    Restructured templates to use a base.html

--- Code/heartland/hl_admin/tests.py
+++ Code/heartland/hl_admin/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+class AdminTestCase(TestCase):
+    def setup(self):
+        exec(open('populate.py').read())
+
+    def testScores():
+
