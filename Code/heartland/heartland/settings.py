26f5f20adfb1838b3fe36ac6bea353df2c6c59fb
Jon Bolin <jonb377@gmail.com>
2017-11-06 22:57:18 -0600

    NOT WORKING. Added localStorage caching.
    Admin page is broken. Local storage is incomplete.

--- Code/heartland/heartland/settings.py
+++ Code/heartland/heartland/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = './static_root/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
--- Code/heartland/hl_admin/tests.py
+++ Code/heartland/hl_admin/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 # Create your tests here.
 class AdminTestCase(TestCase):
     def setup(self):
-        exec(open('populate.py').read())
+        pass
 
     def testScores():
-
+        exec(open('populate.py').read())
